81e69c34ea5e690aa3b9ca835e9873562f81ab65
Mishaine <114750133+Z3R0N1NJ4@users.noreply.github.com>
2023-03-22 11:23:07 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const route = require('./controller');
 // cors
 const cors = require('cors');
 // port 
-const port = parseInt(process.env.PORT) || 9001;
+const port = parseInt(process.env.PORT) || 8081;
 // Express app
 const app = express();
 // Middleware
@@ -17,7 +17,7 @@ bodyParser.urlencoded( {extended: true} ): Object will contain
 values of any type instead of just a string
 */
 app.use((req, res, next)=> {
-        res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+        res.header('Access-Control-Allow-Origin', 'http://localhost:8081')
         res.header("Access-Control-Allow-Credentials", "true")
         res.header("Access-Control-Allow-Methods", "*")
         res.header("Access-Control-Allow-Headers", "*")
